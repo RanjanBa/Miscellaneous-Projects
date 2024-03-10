4ff79d81ab9d95a7fa38f3cbb2b7281f5a7a2ae2
Ranjan Baro <RanjanBa@users.noreply.github.com>
2024-03-10 10:51:16 +0530

    ticktac"

--- TickTac/main.c
+++ TickTac/main.c
@@ -1,76 +1,74 @@
-#include <bits/stdc++.h>
-
-using namespace std;
+#include <stdio.h>
 
 void showBoard(char *a)
 {
     for (int j = 0; j < 3; j++)
     {
-        cout << " __ ";
+        printf(" ___");
     }
 
-    cout << "\n";
+    printf("\n");
 
     for (int i = 0; i < 3; i++)
     {
-        cout << "\n";
+        printf("\n");
         for (int j = 0; j < 3; j++)
         {
-            cout << "| " << a[i * 3 + j] << " ";
+            printf("| %c ", a[i * 3 + j]);
         }
 
-        cout << "|\n";
+        printf("|\n");
 
         for (int j = 0; j < 3; j++)
         {
-            cout << " __ ";
+            printf(" ___");
         }
 
-        cout << "\n";
+        printf("\n");
     }
 }
 
-bool isLegalMove(char *a, int pos)
+int isLegalMove(char *a, int pos)
 {
     if (pos < 0 || pos >= 9)
     {
-        return false;
+        return 0;
     }
 
     if (a[pos] == 'O' || a[pos] == 'X')
     {
-        return false;
+        return 0;
     }
 
-    return true;
+    return 1;
 }
 
-bool checkWin(char *a)
+int checkWin(char *a)
 {
     for (int i = 0; i < 3; i++)
     {
         if (a[i * 3 + 0] == a[i * 3 + 1] && a[i * 3 + 1] == a[i * 3 + 2])
         {
-            return true;
+            return 1;
         }
 
         if (a[i] == a[3 + i] && a[3 + i] == a[6 + i])
         {
-            return true;
+            return 1;
         }
     }
 
     if (a[0] == a[4] && a[4] == a[8])
     {
-        return true;
+        return 1;
     }
 
     if (a[2] == a[4] && a[4] == a[6])
     {
-        return true;
+        return 1;
     }
 
-    return false;
+    return 0;
 }
 
 int main()
@@ -84,30 +82,35 @@ int main()
 
     showBoard(a);
 
-    bool flag = 0;
-    bool isPlaying = true;
+    int flag = 0;
+    int isPlaying = 1;
     while (isPlaying)
     {
-        cout << "\n";
+        printf("\n");
 
         if (flag == 0)
         {
-            cout << "It is player 1 turn...\n";
+            printf("It is player 1 turn...\n");
         }
         else
         {
-            cout << "It is player 2 turn...\n";
+            printf("It is player 2 turn...\n");
         }
 
         int pos = -1;
-        cout << "Possition between 1 to 9 : ";
-        cin >> pos;
+        printf("Possition between 1 to 9 : ");
+        scanf("%d", &pos);
 
         pos--;
 
+        while (getchar() != '\n')
+            ;
+
+        printf("Pos: ", pos);
+
         if (!isLegalMove(a, pos))
         {
-            cout << "Move is illegal. Try another position...\n";
+            printf("Move is illegal. Try another position...\n");
             continue;
         }
 
@@ -117,10 +120,12 @@ int main()
 
         if (checkWin(a))
         {
-            cout << "Player " << (flag == 0 ? "1" : "2") << " won!\n";
+            printf("Player %c won!\n", (flag == 0 ? '1' : '2'));
             break;
         }
 
-        flag = !flag;
+        flag = (flag == 1 ? 0 : 1);
     }
+
+    return 0;
 }
